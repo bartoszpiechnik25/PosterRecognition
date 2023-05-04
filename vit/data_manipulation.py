3c0e61f19294c25be3f0e585e30a63f14a7af3e4
Bartosz Piechnik <bartoszpiechnik25@gmail.com>
2023-05-04 22:36:50 +0200

    Explore transformers ViT model and ViT image processor, create class for loading posters from disk

---
+++ vit/data_manipulation.py
@@ -0,0 +1,125 @@
+import os, sys, shutil, random
+from alive_progress import alive_bar
+from torch.utils.data import Dataset
+from typing import Callable, Tuple
+import torch
+from PIL import Image
+
+def split_train_val_test(path: str) -> None:
+    """
+    Split each class into subdirectories for train, val, and test.
+
+    Args:
+        path (str): Path to directory containing images.
+
+    Raises:
+        AttributeError: If path is not a directory.
+    """
+    if not os.path.isdir(path):
+        print(f"{path} is not a directory!", file=sys.stderr)
+        return
+
+    #get list of posters and shuffle
+    posters = os.listdir(path)
+    random.shuffle(posters)
+    num_posters = len(posters)
+    
+    #create train, val, test splits
+    train_end_idx = int(num_posters * 0.7)
+    val_end_idx = int(num_posters * 0.9)
+
+    split_paths = [os.path.join(path, split)\
+                   for split in ('train', 'val', 'test')]
+    
+    #move posters into split directories
+    for poster_imgs, split in zip([posters[:train_end_idx],
+                            posters[train_end_idx:val_end_idx],
+                            posters[val_end_idx:]], split_paths):
+        os.makedirs(split)
+        for poster in poster_imgs:
+            shutil.move(os.path.join(path, poster),
+                        os.path.join(split, poster))
+            
+
+class PosterDataset(Dataset):
+
+    def __init__(self, datasetRootPath: str,
+                 transform: Callable=None,
+                 split: str='train') -> None:
+        """
+        Initializes a PosterDataset object.
+
+        Args:
+            datasetRootPath (str): Path to directory containing classes.
+
+            transform (Callable, optional): Augumentation function. Defaults to None.
+
+            split (str, optional): Type of split for training. Defaults to 'train'.
+
+        Raises:
+            ValueError: If split is do not indicate train, val, or test split
+            of the dataset.
+        """
+        
+        if split not in ('train', 'val', 'test'):
+            raise ValueError(f"{split} not in (train', val, test)!")
+        
+        self.transform = transform
+
+        self.classToIdx = {cls: idx \
+            for idx, cls in enumerate(sorted(os.listdir(datasetRootPath)))}
+        
+        self.idxToClass = {idx: cls for cls, idx in self.classToIdx.items()}
+
+        self._postersPaths = []
+        for className in self.classToIdx.keys():
+            clsPath: str = os.path.join(self._path, className, split) 
+            dirnames: list =\
+            [[os.path.join(clsPath, poster), self.classToIdx[className]]\
+             for poster in os.listdir(clsPath)]
+            self._postersPaths.extend(dirnames)
+        
+    
+    def __getitem__(self, index: int) -> Tuple[torch.Tensor, int]:
+        """
+        Returns a tuple of image and label.
+
+        Args:
+            index (int): Index from which to return image and label.
+
+        Returns:
+            Tuple[torch.Tensor, int]: Tuple of image and label.
+        """
+        x, y = self._postersPaths[index]
+
+        image = Image.open(x)
+
+        if self.transform:
+            image = self.transform(image)
+        return image, y
+    
+    def __len__(self) -> int:
+        """
+        Returns the number of all posters in the dataset.
+
+        Returns:
+            int: Number of all posters in the dataset.
+        """
+        return len(self._postersPaths)
+
+            
+if __name__ == '__main__':
+    path = "/home/barti/PosterRecognition/scraper/data/images"
+    dirs = os.listdir(path)
+    with alive_bar(len(dirs)) as bar, open(path + "logs.txt", 'w+') as f:
+        for directory in dirs:
+            try:
+                split_train_val_test(os.path.join(path, directory))
+            except:
+                f.write(f"Error occured while processing -> {directory}\n")
+            finally:
+                bar()
+
+            
+
+
