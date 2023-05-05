4dc8337d7bdaaa0f1d81d644efad127604df4f2c
Bartosz Piechnik <bartoszpiechnik25@gmail.com>
2023-05-05 21:09:23 +0200

    Test model with dummy parameters, optimize number of model parmeters from 800M o_O to 100M

--- vit/model.py
+++ vit/model.py
@@ -1,22 +1,22 @@
-from transformers import ViTModel, AutoImageProcessor, ViTConfig
+from transformers import ViTModel, ViTConfig
 from typing import Tuple
 import torch
 import torch.nn.functional as F
 import torch.nn as nn
-import torchvision.transforms as T
 
 class ViTPosterClassifier(nn.Module):
 
-    def __init__(self, num_classes: int=4709) -> None:
+    def __init__(self, last_hidden_size: int = 2048, num_classes: int=4709) -> None:
         super().__init__()
         self.vitConfig = ViTConfig()
         self.vit = ViTModel.from_pretrained('google/vit-base-patch16-224-in21k')
-        self.fullyConnected = nn.Sequential(nn.ModuleList([
-            nn.LazyLinear(num_classes),
+        self.avgPool = nn.AdaptiveAvgPool1d(1)
+        self.fullyConnected = nn.Sequential(
+            nn.Linear(self.vitConfig.hidden_size, last_hidden_size),
             nn.GELU(),
-            nn.Dropout(0.2)
-        ]))
-        self.criterion = nn.CrossEntropyLoss()
+            nn.Dropout(0.3),
+            nn.Linear(last_hidden_size, num_classes),
+        )
     
     def forward(self, x: torch.Tensor, y: torch.Tensor=None) -> Tuple[torch.Tensor, torch.Tensor]:
         """
@@ -29,12 +29,14 @@ class ViTPosterClassifier(nn.Module):
         Returns:
             Tuple[torch.Tensor, torch.Tensor]: Tuple of logits and loss.
         """
-        batch_size = x.shape[0]
-        x = self.vit.forward(x,
+        batch = x.shape[0]
+        x: torch.Tensor = self.vit.forward(x,
                             return_dict=False,
                             output_attentions=False,
                             output_hidden_states=False)[0]
-        logits = self.fullyConnected(x.view(batch_size, -1))
+        
+        x = self.avgPool(x.permute(0, 2, 1)).view(batch, -1)
+        logits = self.fullyConnected(x)
 
         loss = None
         if y is not None:
@@ -50,7 +52,7 @@ class ViTPosterClassifier(nn.Module):
         return prediction
     
     @classmethod
-    def from_pretrained(cls, path: str, num_classes: int=4709) -> nn.Module:
+    def from_pretrained(cls, path: str) -> nn.Module:
         """
         Loads a pretrained model from a state dict.
 
@@ -61,6 +63,30 @@ class ViTPosterClassifier(nn.Module):
         Returns:
             nn.Module: Model with loaded weights.
         """
-        model = cls(num_classes)
-        model.load_state_dict(torch.load(path))
+        model = torch.load(path)
         return model
+    
+    def saveWeights(self, path: str=None) -> None:
+        """
+        Saves the model weights.
+
+        Args:
+            path (str): Path to save weights.
+        """
+        if path is None:
+            path = f"/home/barti/PosterRecognition/vit/checkpoints/{self._get_name()}_state_dict.pth"
+        print(f"Saving model to ---> {path}")
+        torch.save(self, path)
+
+
+if __name__ == '__main__':
+    model = ViTPosterClassifier()
+    print(f"Model has --> {sum(p.numel() for p in model.parameters() if p.requires_grad):,} parameters.")
+    print(model._get_name())
+    dummy_batch = torch.randn(8, 3, 224, 224, device='cuda')
+    dummy_target = torch.randint(0, 4709, (8,), device='cuda')
+    model.to('cuda')
+    logits, loss = model.forward(dummy_batch, dummy_target)
+    loss.backward()
+    model.saveWeights()
+    print(logits.shape)
